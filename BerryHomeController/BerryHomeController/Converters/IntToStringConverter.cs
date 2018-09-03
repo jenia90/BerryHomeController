03cc39f47bc4dc424af6884323ba49347b1ea034
Jenia Dysin <genia90@gmail.com>
2018-09-03 12:01:56 +0300

    Added IntToStringConverter and some type conversions in the ExapndedDevicePage. Gotta figure out what's causing the ArrayTypeMismatchException.

---
+++ BerryHomeController/BerryHomeController/Converters/IntToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BerryHomeController.Common.Converters
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int) value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse((string) value);
+        }
+    }
+}
